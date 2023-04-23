5ae759b5eca01e96bc778e247ff27e38bdf5296e
Tbence98 <tlrbence@gmail.com>
2023-04-23 16:48:26 +0200

    make AddTaskForm scrollable

--- components/AddTaskForm/AddTaskForm.js
+++ components/AddTaskForm/AddTaskForm.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, ScrollView } from "react-native";
 
 import Input from "../Input/Input";
 import PrimaryReactiveButton from "../UI/PrimaryReactiveButton/PrimaryReactiveButton";
@@ -80,87 +80,94 @@ const AddTaskForm = ({ onSubmit, defaultValues }) => {
     }
 
     return (
-        <View className="mt-12 relative flex-1">
-            {activeCalendar ? (
-                <DateTimePicker
-                    mode="date"
-                    display="default"
-                    value={new Date(inputs[activeCalendar].value)}
-                    onChange={dateChangeHandler.bind(null, activeCalendar)}
-                />
-            ) : null}
-            <View className="flex-row justify-between">
-                <Pressable
-                    onPress={() => setActiveCalendar("startDate")}
-                    className="flex-1"
-                >
-                    <Input
-                        label="Start"
-                        invalid={!inputs.startDate.isValid}
-                        textInputConfig={{
-                            placeholder: formatDate(inputs.startDate.value),
-                            editable: false,
-                        }}
+        <View className="mt-12 flex-1">
+            <ScrollView className="flex-[5]">
+                {activeCalendar ? (
+                    <DateTimePicker
+                        mode="date"
+                        display="default"
+                        value={new Date(inputs[activeCalendar].value)}
+                        onChange={dateChangeHandler.bind(null, activeCalendar)}
                     />
-                </Pressable>
-                <Pressable
-                    onPress={() => setActiveCalendar("endDate")}
-                    className="flex-1"
-                >
-                    <Input
-                        label="End"
-                        invalid={!inputs.endDate.isValid}
-                        textInputConfig={{
-                            placeholder: formatDate(inputs.endDate.value),
-                            editable: false,
-                        }}
-                    />
-                </Pressable>
-            </View>
-            <Input
-                label="Title"
-                invalid={!inputs.title.isValid}
-                textInputConfig={{
-                    onChangeText: inputChangedHandler.bind(null, "title"),
-                    value: inputs.title.value,
-                }}
-            />
-            <View className="my-2 mx-1">
-                <Text className="text-sm font-bold text-blue-600 mb-1">
-                    Category
-                </Text>
+                ) : null}
                 <View className="flex-row justify-between">
-                    <PrimaryReactiveButton
-                        isActive={category === "priority"}
-                        onPress={categoryChangedHandler.bind(null, "priority")}
-                        style="flex-1"
+                    <Pressable
+                        onPress={() => setActiveCalendar("startDate")}
+                        className="flex-1"
                     >
-                        Priority Task
-                    </PrimaryReactiveButton>
-                    <PrimaryReactiveButton
-                        isActive={category === "daily"}
-                        onPress={categoryChangedHandler.bind(null, "daily")}
-                        style="flex-1"
+                        <Input
+                            label="Start"
+                            invalid={!inputs.startDate.isValid}
+                            textInputConfig={{
+                                placeholder: formatDate(inputs.startDate.value),
+                                editable: false,
+                            }}
+                        />
+                    </Pressable>
+                    <Pressable
+                        onPress={() => setActiveCalendar("endDate")}
+                        className="flex-1"
                     >
-                        Daily Task
-                    </PrimaryReactiveButton>
+                        <Input
+                            label="End"
+                            invalid={!inputs.endDate.isValid}
+                            textInputConfig={{
+                                placeholder: formatDate(inputs.endDate.value),
+                                editable: false,
+                            }}
+                        />
+                    </Pressable>
                 </View>
+                <Input
+                    label="Title"
+                    invalid={!inputs.title.isValid}
+                    textInputConfig={{
+                        onChangeText: inputChangedHandler.bind(null, "title"),
+                        value: inputs.title.value,
+                    }}
+                />
+                <View className="my-2 mx-1">
+                    <Text className="text-sm font-bold text-blue-600 mb-1">
+                        Category
+                    </Text>
+                    <View className="flex-row justify-between">
+                        <PrimaryReactiveButton
+                            isActive={category === "priority"}
+                            onPress={categoryChangedHandler.bind(
+                                null,
+                                "priority"
+                            )}
+                            style="flex-1"
+                        >
+                            Priority Task
+                        </PrimaryReactiveButton>
+                        <PrimaryReactiveButton
+                            isActive={category === "daily"}
+                            onPress={categoryChangedHandler.bind(null, "daily")}
+                            style="flex-1"
+                        >
+                            Daily Task
+                        </PrimaryReactiveButton>
+                    </View>
+                </View>
+                <Input
+                    label="Description"
+                    invalid={!inputs.description.isValid}
+                    textInputConfig={{
+                        multiline: true,
+                        onChangeText: inputChangedHandler.bind(
+                            null,
+                            "description"
+                        ),
+                        value: inputs.description.value,
+                    }}
+                />
+            </ScrollView>
+            <View className="flex-1 justify-end">
+                <PrimaryButton style="mb-4" onPress={submitHandler}>
+                    Create Task
+                </PrimaryButton>
             </View>
-            <Input
-                label="Description"
-                invalid={!inputs.description.isValid}
-                textInputConfig={{
-                    multiline: true,
-                    onChangeText: inputChangedHandler.bind(null, "description"),
-                    value: inputs.description.value,
-                }}
-            />
-            <PrimaryButton
-                style="absolute bottom-3 left-0 right-0"
-                onPress={submitHandler}
-            >
-                Create Task
-            </PrimaryButton>
         </View>
     );
 };
